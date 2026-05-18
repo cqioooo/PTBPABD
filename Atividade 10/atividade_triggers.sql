cc1e10fb165feef758206fb554e3b354355c4bf6
cqioooo <165922359+cqioooo@users.noreply.github.com>
2026-05-18 17:01:46 -0300

    Adicionando atividade 10

---
+++ Atividade 10/atividade_triggers.sql
@@ -0,0 +1,17 @@
+GO
+
+CREATE TRIGGER dbo.gain_credits
+ON dbo.takes
+AFTER INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    -- Incrementa os créditos do aluno com base no curso que ele acabou de inserir
+    UPDATE s
+    SET s.tot_cred = s.tot_cred + c.credits
+    FROM dbo.student AS s
+    INNER JOIN inserted AS i ON s.ID = i.ID
+    INNER JOIN dbo.course AS c ON i.course_id = c.course_id;
+END;
+GO
